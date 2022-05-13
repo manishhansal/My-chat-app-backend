a718f255356fc1b7c92d633401c9d4faf1b0f2b5
Manish Kumar <manishhansalmeh@gmail.com>
2022-05-13 16:31:37 +0530

    update

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://chatApp:${process.env.DB_PW}@cluster0.mgnrv.mongodb.net/chatApp?retryWrites=true&w=majority`, () => {
+mongoose.connect( process.env.MONGODB_URI || `mongodb+srv://chatApp:${process.env.DB_PW}@cluster0.mgnrv.mongodb.net/chatApp?retryWrites=true&w=majority`, () => {
     console.log("Connected to mongoDB");
 });
